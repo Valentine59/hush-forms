32e4b06a89b5f220bff35896a1940660ca1b250a
Valentine59 <MarshallGraciebwlat@outlook.com>
2025-11-03 10:16:43 +0800

    chore(test): improve contract security

--- test/EncryptedSurvey.ts
+++ test/EncryptedSurvey.ts
@@ -1,3 +1,4 @@
+// Auto-generated comment for collaboration - 2025-11-08 16:39:17
 // Auto-generated comment for collaboration - 2025-11-08 16:39:16
 // Auto-generated comment for collaboration - 2025-11-08 16:39:15
 import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
